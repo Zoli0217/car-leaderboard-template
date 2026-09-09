b91b5a10f6bab0c26bce59a7116b24d1be487708
Zoli0217 <zoltanbodi61@gmail.com>
2026-09-09 09:02:30 +0200

    fetch

--- src/components/Podium.jsx
+++ src/components/Podium.jsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react"
+
 const Podium = () => {
+  const [podium, setPodium] = useState([])
+
+  useEffect(() => {
+    fetch("podium.json")
+    .then((response) => response.json())
+    
+  })
+
   return (
     <div>Podium</div>
   )
